b33ec8d390af492c2ea11247f9e48de3ae20e352
muyevsyong <50413795+muyevsyong@users.noreply.github.com>
2024-07-17 13:36:29 +0800

    Add files via upload

--- lstm_t2well.py
+++ lstm_t2well.py
@@ -1,8 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[75]:
+
+
 import datetime
 import warnings
 import re
 import gc
-import os
+import os    
+
+#import pickle
 
 import tensorflow as tf
 import numpy as np
@@ -11,9 +19,19 @@ from matplotlib import pyplot as plt
 import seaborn as sns
 from tensorflow.keras.optimizers.schedules import PiecewiseConstantDecay
 
+
+# In[76]:
+
+
 gpu = tf.config.experimental.list_physical_devices(device_type='GPU')
 assert len(gpu) == 1
 tf.config.experimental.set_memory_growth(gpu[0], True)
+
+
+# # hyperparameters
+
+# In[77]:
+
 
 settings = {
     "units": 128,
@@ -24,11 +42,17 @@ settings = {
     "epochs_num":1000,
 }
 
-path = "../input/tough-lstm/"
+
+# # Data
+
+# In[78]:
+
+path = "../input/lstm100num/input/"
 file_list = os.listdir(path)
 trn_data = []
 val_data = []
 time_steps = 11
+print(len(file_list))
 
 trn_size = int(len(file_list) * 0.8)
 for file in file_list[:trn_size]:
@@ -40,13 +64,20 @@ for file in file_list[trn_size:]:
     data_tmp = np.genfromtxt(path + file).T
     for row in range(len(data_tmp) - time_steps):
         val_data.append(np.expand_dims(data_tmp[row:row + time_steps], axis=0))
-        
+
+
+# In[79]:
+
 
 trn_data = np.concatenate(trn_data, axis=0)
 val_data = np.concatenate(val_data, axis=0)
 
 num_trn, _, num_feat = trn_data.shape
 num_val, _, _ = val_data.shape
+
+
+# In[80]:
+
 
 trn_data = trn_data.reshape([-1, num_feat])
 val_data = val_data.reshape([-1, num_feat])
@@ -59,7 +90,15 @@ val_data = (val_data - trn_mean) / trn_std
 trn_data = trn_data.reshape([num_trn, time_steps, num_feat])
 val_data = val_data.reshape([num_val, time_steps, num_feat])
 
+
+# In[81]:
+
+
 settings["trg_num"] = trn_data.shape[-1]
+
+
+# In[82]:
+
 
 trn_x = trn_data[:, :time_steps]
 trn_y = trn_data[:, -1]
@@ -69,14 +108,27 @@ val_y = val_data[:, -1]
 
 settings["input_shape"] = trn_x.shape[1:]
 
+
+# In[83]:
+
+
 def make_dataset(feature, y, batch_size=1024):
     ds = tf.data.Dataset.from_tensor_slices((feature, y))
     ds = ds.shuffle(2*batch_size).batch(batch_size).cache().prefetch(tf.data.experimental.AUTOTUNE)
     return ds
-    
+
+
+# In[84]:
+
 
 trn_ds = make_dataset(trn_x, trn_y)
-val_ds = make_daztaset(val_x, val_y)
+val_ds = make_dataset(val_x, val_y)
+
+
+# # Model
+
+# In[85]:
+
 
 def get_model(settings):
     seq = []
@@ -110,10 +162,17 @@ def get_model(settings):
         optimizer=tf.keras.optimizers.Adam(learning_rate=settings["lr"]),
         loss="mse")
     return model
-    
-    
+
+
+# In[86]:
+
+
 model = get_model(settings)
 model.summary()
+
+
+# In[87]:
+
 
 early_stopping = tf.keras.callbacks.EarlyStopping(
     monitor='val_loss', patience=settings["patience"], mode='min')
@@ -133,13 +192,22 @@ history = model.fit(trn_ds,
 
 model.load_weights("./checkpoint")
 
+
+# # Validation
+
+# In[88]:
+
+
 def myMase(pred, true, m=1):
     '''m是周期长度'''
     return np.mean(np.abs(pred-true))/np.mean(np.abs(true[m:] - true[:-m]))
 
 def myRmse(pred, true):
     return np.sqrt(np.mean(np.square(pred-true)))
-    
+
+
+# In[89]:
+
 
 pd.DataFrame(history.history)[[
     'loss', 'val_loss'
@@ -151,6 +219,10 @@ plt.ylabel('MSE', fontsize=15)
 #plt.title('9-16-0.0063-500',fontsize=25)
 plt.show()
 
+
+# In[90]:
+
+
 pred_trn = model.predict(trn_x, verbose=0)
 mase1 = myMase(pred_trn, trn_y)
 print(mase1)
@@ -158,9 +230,13 @@ rmse1 = myRmse(pred_trn, trn_y)
 print(rmse1)
 print(myRmse(trn_y[:-1], trn_y[1:]))
 
-for i, feat in enumerate(['a','b']):
+
+# In[91]:
+
+
+for i, feat in enumerate(['TTDspSS']):
     tmp = pd.DataFrame(
-        np.concatenate([pred_val[:100, i:i + 1], val_y[:100, i:i + 1]], axis=1))
+        np.concatenate([pred_trn[:1000, i:i + 1], trn_y[:1000, i:i + 1]], axis=1))
     tmp.columns = ['pred', 'true']
 
     tmp.plot(figsize=(18, 4), color=['r', 'b'])
@@ -172,18 +248,61 @@ for i, feat in enumerate(['a','b']):
     plt.title(feat, fontsize=20)
     plt.legend(fontsize=15)
     plt.show()
-    
-i = 98
-tmp = pd.DataFrame(
-    np.concatenate([pred_val[:100, i:i + 1], val_y[:100, i:i + 1]], axis=1))
-tmp.columns = ['pred', 'true']
 
-tmp.plot(figsize=(18, 4), color=['r', 'b'])
-plt.grid(True)
-plt.xlabel('epoch', fontsize=15)
-plt.ylabel('value', fontsize=15)
-plt.xticks(fontsize=15)
-plt.yticks(fontsize=15)
-plt.title(feat, fontsize=20)
-plt.legend(fontsize=15)
-plt.show()
+
+# In[92]:
+
+
+pred_val = model.predict(val_x, verbose=0)
+mase2 = myMase(pred_val, val_y)
+print(mase2)
+rmse2 = myRmse(pred_val, val_y)
+print(rmse2)
+print(myRmse(val_y[:-1], val_y[1:]))
+
+
+# **保存**
+
+# In[93]:
+
+
+tf.saved_model.save(model, './')     #保存模型
+
+#def save_variable(v,filename):
+#    f=open(filename,'wb')          #打开或创建名叫filename的文档。
+#    pickle.dump(v,f)               #在文件filename中写入v
+#    f.close()                      #关闭文件，释放内存。
+#    return filename
+
+#save_variable(pred_trn,'./pred_trn0')
+#save_variable(trn_y,'./trn_y0')
+#save_variable(pred_val,'./pred_val0')
+#save_variable(val_y,'./val_y0')
+#保存数据
+pred_trn0= pd.DataFrame(
+        np.concatenate([pred_trn], axis=1))
+pred_trn0.to_csv('./pred_tr.csv')
+
+trn_y0= pd.DataFrame(
+        np.concatenate([trn_y], axis=1))
+trn_y0.to_csv('./true_tr0.csv')
+
+pred_val0= pd.DataFrame(
+        np.concatenate([pred_val], axis=1))
+pred_val0.to_csv('./pred_test0.csv')
+
+val_y0= pd.DataFrame(
+        np.concatenate([val_y], axis=1))
+val_y0.to_csv('./test_y0.csv')
+
+meandata= pd.DataFrame(
+        np.concatenate([trn_mean,trn_std], axis=0))
+meandata.to_csv('./meandata.csv')
+
+print(trn_std)
+
+
+
+
+
+
